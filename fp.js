5704f7304fc7c87ddec1fe13254188bf472386b0
Samuel <samuelrivba@gmail.com>
2024-04-11 14:50:40 -0500

    Add idempotence

--- fp.js
+++ fp.js
@@ -4,10 +4,19 @@
 
 function a() {
   console.log("Hi");
-} // Thishas side effects
+} // This has side effects
 
 function b(num1, num2) {
   return num1 + num2;
 }
 
 a(3, 4); // This always return the same, referencial transparency is that we could change this to 7
+
+// Functional programming aims to minimize side effects
+
+// Idempotence always the same returns
+function notGood() {
+  return Math.random(num);
+}
+
+notGood(5);
