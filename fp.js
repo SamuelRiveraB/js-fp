3eb9beacea71900e514bf6d58bed2cc82671d869
Samuel <samuelrivba@gmail.com>
2024-04-11 15:24:52 -0500

    Add Partial application

--- fp.js
+++ fp.js
@@ -73,3 +73,8 @@ const curriedMultiply = (a) => (b) => a * b;
 const curriedMultiplyBy5 = curriedMultiply(5);
 
 console.log(curriedMultiplyBy5(4));
+
+// Partial application
+const multiply2 = (a, b, c) => a * b * c;
+const partialMultiplyBy5 = multiply2.bind(null, 5);
+console.log(partialMultiplyBy5(4, 10));
